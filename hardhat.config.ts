1647c061918fe1d3ea6d9471e766cb6cb3e1feab
leon-do <leondo123@gmail.com>
2024-03-09 20:07:50 -0500

    yarn emuswap

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,6 @@ import '@nomiclabs/hardhat-etherscan'
 import '@nomiclabs/hardhat-waffle'
 import 'hardhat-typechain'
 import 'hardhat-watcher'
-import 'dotenv/config'
 
 const DEFAULT_COMPILER_SETTINGS = {
   version: '0.7.6',
